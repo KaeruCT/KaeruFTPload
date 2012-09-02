ce7c3f9b92447ad7b4ad982bd4f977e7d054421a
Andres V <darklink898@gmail.com>
2012-09-01 20:40:07 -0600

    fixing what tribal's pull request broke

--- kaeruftpload/ftp_uploader.py
+++ kaeruftpload/ftp_uploader.py
@@ -12,32 +12,34 @@ class ftp_uploader:
 		self.password = password
 		self.directory = directory
 		self.url = url
-	
+
 	def upload_files(self, file_names):
 		log = []
-		
+
 		try:
 			ftp = FTP(self.host, self.user, self.password)
 			ftp.cwd(self.directory)
 		except:
 			ftp.quit()
 			return 0
-		
+
 		try:
 			for x in file_names:
 				full_name = os.path.abspath(x)
 				file = open(full_name, "rb")
-				
+
 				file_hash = str(crc32(file.read(1048576))) # Read in 1MB
-				file_name = "[{0}]{1}".format(file_hash[:3], basename(file_name))
-		
+				file.seek(0)
+
+				file_name = "{0}-{1}".format(file_hash[:3], os.path.basename(full_name))
+
 				result = self.url + urllib.quote(file_name) + "\n";
 				result += ftp.storbinary("STOR " + file_name, file)
 				log.append(result)
 		except:
 			ftp.quit()
 			return 1
-			
+
 		ftp.quit()
-		
+
 		return "\n".join(log)
